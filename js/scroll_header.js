2182d86ca41c08d7df2d9a883547ff22318d5c0c
Lev Khomchenkov <pokemon9116@yandex.ru>
2020-05-14 15:50:11 +0700

    mb final fix

--- js/scroll_header.js
+++ js/scroll_header.js
@@ -1,7 +1,7 @@
 window.onscroll = function showHeader() {
     let header = document.querySelector('.header-scroll')
 
-    if(window.pageYOffset > 15){
+    if(window.pageYOffset > 75){
         header.classList.add('header-fixed');
     } else 
         header.classList.remove('header-fixed')};
